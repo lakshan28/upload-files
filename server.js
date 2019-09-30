990fd9bd36ab3104dbf4326b46770ceb11657729
lakshan28 <lakshansampath28@gmail.com>
2019-09-30 08:07:22 +0530

    progress

--- server.js
+++ server.js
@@ -5,24 +5,22 @@ const app = express();
 
 app.use(fileUpload());
 
+// Upload Endpoint
 app.post("/upload", (req, res) => {
   if (req.files === null) {
-    return res.status(400).json({ msg: "No file upload" });
+    return res.status(400).json({ msg: "No file uploaded" });
   }
 
   const file = req.files.file;
 
-  file.mv(`${__dirname}/client/src/uploads/${file.name}`, err => {
+  file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
     if (err) {
       console.error(err);
-
       return res.status(500).send(err);
     }
 
-    res.json({ fileName: file.name, filePath: `./uploads/${file.name}` });
+    res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
   });
 });
 
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(5000, () => console.log("Server Started..."));
